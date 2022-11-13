f29b642475137df74448fdf425df7b69172a2a7b
seohyun9672 <ajeong4@my.bcit.ca>
2022-11-12 18:59:25 -0800

    change variables with data from the table

--- client/pages/result.js
+++ client/pages/result.js
@@ -1,38 +1,69 @@
-import { useEffect, useRouter, useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Result() {
 
-  const [signs, setSigns] = useState([]);
-  const [sign, setSign] = useState('');
-
-  const [names, setNames] = useState([]);
   const [name, setName] = useState('');
 
-  // const r = useRouter();
+  const [signName, setSignName] = useState("");
+  const [src, setSrc] = useState("");
+  const [dateStart, setDateStart] = useState("");
+  const [dateEnd, setDateEnd] = useState("");
+  const [description, setDescription] = useState("");
+
+  const r = useRouter();
 
   useEffect(() => {
     // get current names from backend
     fetch('http://localhost:3001/get-name')
       .then(async (res) => {
         const data = await res.json();
-        var nameArr = data.names
-        let name = nameArr.slice(-1);
-        setName(name)
-      })
+        var nameArr = data.names;
+        var name = nameArr.slice(-1);
+        setName(name);
+      });
   });
 
-  return <div>
-    <h1>{name},</h1>
-    {
-      signs.map((data) => {
-        <div>
-          <h1>You are a {data.sign}!</h1>
-          <img src={data.src}></img>
-          <div>{data.startDate} - {data.endDate}</div>
-          <div>{data.description}</div>
-        </div>
-      })
-    }
+  // this is only for the display purpose
+  useEffect(() => {
+    changeSign();
+  }, []);
 
+  const changeSign = () => {
+    fetch(`http://localhost:3001/get-signs?sign-name=${SignName}`)
+      .then(async (res) => {
+        const data = await res.json();
+        const signArr = data.signs;
+        var i = 0;
+
+        var SignName = signArr[i].SignName;
+        var ImgSrc = signArr[i].ImgSrc;
+        var Description = signArr[i].Description;
+        var DateStart = signArr[i].DateStart;
+        var DateEnd = signArr[i].DateEnd;
+
+        // maybe we can try this way? (idk if this will be working)
+        // if ( we need a condition here where it indicates the date range) {
+        //   // i = 1
+        //   // so it changes index number of the signArr so it detects the index number of the sign arr info and returns the right sign info
+        // }
+
+        setSignName(SignName);
+        setDateEnd(DateEnd);
+        setDateStart(DateStart);
+        setSrc(ImgSrc);
+        setDescription(Description)
+      });
+  };
+
+  return <div>
+    <button onClick={() => {
+      r.push("/")
+    }}>Back to Main</button>
+    <h1>{name},</h1>
+    <h1>You are a {signName}!</h1>
+    <img src={src}></img>
+    <div>{dateStart} - {dateEnd}</div>
+    <div>{description}</div>
   </div>
-}
+};
