e48a77a80e400f50c5be7dbdf59d434dcaa415b9
seohyun9672 <ajeong4@my.bcit.ca>
2022-11-13 18:01:04 -0800

    add styling

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,29 @@
 import { useRouter } from "next/router";
 
+
 export default function Home() {
   const r = useRouter();
 
   return (
-    <div>
-      <h1>Star Wheel</h1>
-      <h2>Horoscope Calculator</h2>
-      <button onClick={() => r.push("input")}>Begin</button>
-    </div>
+    <>
+      <div className="container center">
+        <div className="imgcont">
+          <img src="/windRose.svg" />
+        </div>
+        <div className="textcont center">
+          <h1>Star Wheel</h1>
+          <h2>Horoscope Calculator</h2>
+        </div>
+      </div>
+      <div className="btncont center">
+        <button
+          className="begin"
+          onClick={
+            () => r.push({
+              pathname: "/input",
+              query: "name"
+            })}>Begin</button>
+      </div>
+    </>
   );
 }
